cf9543e60cbce1e68e4a5b81d0a824117293c967
Samuele <marelli.sml@gmail.com>
2021-08-21 17:51:38 +0200

    Aggiunto file style.css Aggiunto le dipendenze ikonli Aggiunta interfaccia iniziale

--- src/main/java/it/uninsubria/centrivaccinali/CentriVaccinali.java
+++ src/main/java/it/uninsubria/centrivaccinali/CentriVaccinali.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class CentriVaccinali extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(CentriVaccinali.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(CentriVaccinali.class.getResource("fxml/Avvio.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Centri Vaccinali");
         stage.setScene(scene);
         stage.show();
     }
