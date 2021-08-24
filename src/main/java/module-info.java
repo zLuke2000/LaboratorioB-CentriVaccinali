bfb6fecb4d1f7b79c0a174a176628536a68959e0
zLuke2000 <luca100.lc@gmail.com>
2021-08-24 16:31:09 +0200

    aggiornato module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,12 +3,15 @@ module it.uninsubria.laboratoriob {
     requires javafx.fxml;
 
     requires org.controlsfx.controls;
-    requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
+    requires org.kordamp.ikonli.javafx;
     requires org.kordamp.ikonli.bpmn;
     requires org.kordamp.ikonli.carbonicons;
+    requires org.kordamp.ikonli.elusive;
     requires java.sql;
 
     exports it.uninsubria.laboratoriob.client;
     opens it.uninsubria.laboratoriob.client to javafx.fxml;
+    exports it.uninsubria.laboratoriob.client.centrivaccinali.controller;
+    opens it.uninsubria.laboratoriob.client.centrivaccinali.controller to javafx.fxml;
 }
