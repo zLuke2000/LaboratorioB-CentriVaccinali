0e16496f642184bad9be5d7e22aa2c2bc5d8bd0c
Samuele <marelli.sml@gmail.com>
2021-08-24 15:22:16 +0200

    Terminata interfaccia CV_login.fxml Sistemato lostile style.css Importate librerie in pom.xml

--- src/main/java/it/uninsubria/laboratoriob/client/ClientCV.java
+++ src/main/java/it/uninsubria/laboratoriob/client/ClientCV.java
@@ -8,6 +8,9 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class ClientCV extends Application {
+
+
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(ClientCV.class.getResource("fxml/Avvio.fxml"));
