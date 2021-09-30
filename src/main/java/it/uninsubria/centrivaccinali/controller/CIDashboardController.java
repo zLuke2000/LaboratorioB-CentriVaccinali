6e395702d0cdcb32d26c55447b314fcdba2a554e
Samuele <marelli.sml@gmail.com>
2021-09-30 17:57:02 +0200

    Inizio CI_dashboard.fxml e  CI_infoCV.fxml Inserita dependecy ikonli, modificati e inseriti stili in style.css

---
+++ src/main/java/it/uninsubria/centrivaccinali/controller/CIDashboardController.java
@@ -0,0 +1,12 @@
+package it.uninsubria.centrivaccinali.controller;
+
+import it.uninsubria.centrivaccinali.models.Cittadino;
+import javafx.event.ActionEvent;
+
+public class CIDashboardController {
+
+    private Cittadino CcittadinoConesso = null;
+
+    public void cercaCentroVaccinale(ActionEvent actionEvent) {
+    }
+}
