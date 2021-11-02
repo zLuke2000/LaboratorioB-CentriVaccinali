94ba0191d6e2d664511e4d5fd80c47d5e2f0525f
zLuke2000 <luca100.lc@gmail.com>
2021-11-02 18:48:38 +0100

    Sistemata interfaccia segnalazione eventi avversi (PC)

--- src/main/java/it/uninsubria/centrivaccinali/models/Result.java
+++ src/main/java/it/uninsubria/centrivaccinali/models/Result.java
@@ -69,6 +69,8 @@ public class Result implements Serializable {
     public enum Error {
         // Login utente
         USERNAME_NON_TROVATO, PASSWORD_ERRATA,
+        //Registrazione eventi avversi
+        EVENTO_GIA_INSERITO,
         //Registrazione utente
         CF_NON_VALIDO, IDVAC_NON_VALIDO, CF_ID_NON_VALIDI, CITTADINO_GIA_REGISTRATO, EMAIL_GIA_IN_USO, USERID_GIA_IN_USO,
         //Registrazione vaccinato
