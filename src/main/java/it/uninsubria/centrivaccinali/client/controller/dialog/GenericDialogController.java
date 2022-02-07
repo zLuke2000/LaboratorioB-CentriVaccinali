7142903cda0dbfe10316ea4cdcf003adc8892d13
liuk <llattarulo@studenti.uninsubria.it>
2022-02-07 11:19:38 +0100

    modifica javadoc

--- src/main/java/it/uninsubria/centrivaccinali/client/controller/dialog/GenericDialogController.java
+++ src/main/java/it/uninsubria/centrivaccinali/client/controller/dialog/GenericDialogController.java
@@ -15,6 +15,10 @@ import javafx.scene.layout.FlowPane;
 
 /**
  * Controller per dialog generici
+ * @author Centore Luca 740951
+ * @author Lattarulo Luca 742597
+ * @author Marelli Samuele
+ * @author Pintonello Christian 741112
  */
 public class GenericDialogController {
 
